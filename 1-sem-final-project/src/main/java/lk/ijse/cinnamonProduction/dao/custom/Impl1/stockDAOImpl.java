a25bf8271bdc11152b014f58a5b3b8daac5eebf4
sachiniwathudura <sachiniwathudura@gmail.com>
2024-01-12 10:59:46 +0530

    stockDAO complete

--- 1-sem-final-project/src/main/java/lk/ijse/cinnamonProduction/dao/custom/Impl1/stockDAOImpl.java
+++ 1-sem-final-project/src/main/java/lk/ijse/cinnamonProduction/dao/custom/Impl1/stockDAOImpl.java
@@ -1,22 +1,20 @@
 package lk.ijse.cinnamonProduction.dao.custom.Impl1;
 
+import lk.ijse.cinnamonProduction.dao.SQLUtil;
 import lk.ijse.cinnamonProduction.dao.custom.satockDAO;
-import lk.ijse.cinnamonProduction.db.DbConnection;
+import lk.ijse.cinnamonProduction.dto.cinnamonGradesDto;
 import lk.ijse.cinnamonProduction.entity.stock;
-import lk.ijse.cinnamonProduction.viewTDM.stockTm;
 //import lk.ijse.cinnamonProduction.dto.stock;
 //import lk.ijse.cinnamonProduction.dto.tm.stockTm;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class stockModel implements satockDAO {
+public class stockDAOImpl implements satockDAO {
 
-    public static boolean saveItem(stockTm stockDto) throws SQLException {
+    /*public static boolean saveItem(stockTm stockDto) throws SQLException {
         Connection connection = DbConnection.getInstance().getConnection();
         String sql = "INSERT INTO stock VALUES(?, ?, ?, ?)";
         PreparedStatement pstm = connection.prepareStatement(sql);
@@ -100,5 +98,73 @@ public class stockModel implements satockDAO {
             dto = new stock(itemId,itemName,itemQty,itemCategory);
         }
         return dto;
+    }*/
+
+    @Override
+    public List<stock> getAll() throws SQLException, ClassNotFoundException {
+       // String sql = "SELECT * FROM stock";
+      //  PreparedStatement pstm = connection.prepareStatement(sql);
+
+        List<stock> dtoList = new ArrayList<>();
+
+        ResultSet resultSet = SQLUtil.execute("SELECT * FROM stock");
+
+        while (resultSet.next()) {
+            String itemId = resultSet.getString(1);
+            String itemName= resultSet.getString(2);
+            double itemQty= Double.parseDouble(resultSet.getString(3));
+            String itemCategory= resultSet.getString(4);
+
+            var dto = new stock(itemId,itemName,itemQty,itemCategory);
+            dtoList.add(dto);
+        }
+        return dtoList;
+    }
+
+    @Override
+    public boolean save(stock entity) throws SQLException, ClassNotFoundException {
+        return SQLUtil.execute("INSERT INTO stock VALUES(?, ?, ?, ?)",
+                entity.getItemId(),entity.getItemName(),entity.getItemQty(),entity.getItemCategory());
+    }
+
+    @Override
+    public boolean update(stock entity) throws SQLException, ClassNotFoundException {
+        return SQLUtil.execute("UPDATE stock SET itemName = ?, itemQty = ?, itemCategory = ? WHERE itemId = ?",
+                entity.getItemName(),entity.getItemQty(),entity.getItemCategory(),entity.getItemId());
+    }
+
+    @Override
+    public boolean exist(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) throws SQLException, ClassNotFoundException {
+        SQLUtil.execute("DELETE FROM stock WHERE itemId = ?",id);
+        return false;
+    }
+
+    @Override
+    public String generateID() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public stock search(String id) throws SQLException, ClassNotFoundException {
+        //String sql = "SELECT * FROM stock WHERE itemId = ?";
+
+        ResultSet resultSet = SQLUtil.execute("SELECT * FROM stock WHERE itemId = ?",id);
+
+        stock entity = null;
+        if(resultSet.next()){
+
+            String itemId = resultSet.getString(1);
+            String itemName = resultSet.getString(2);
+            double itemQty  = Double.parseDouble(resultSet.getString(3));
+            String itemCategory = resultSet.getString(4);
+
+            entity = new stock(itemId,itemName,itemQty,itemCategory);
+        }
+        return entity;
     }
 }
