9f1aa764f6a5d7703cad26a15a05c9bd0ef2b65b
sachiniwathudura <sachiniwathudura@gmail.com>
2024-01-12 11:05:07 +0530

    all complete

---
+++ 1-sem-final-project/src/main/java/lk/ijse/cinnamonProduction/dao/custom/userLoginDAO.java
@@ -0,0 +1,7 @@
+package lk.ijse.cinnamonProduction.dao.custom;
+
+import lk.ijse.cinnamonProduction.dao.CrudDAO;
+import lk.ijse.cinnamonProduction.entity.userLogin;
+
+public interface userLoginDAO extends CrudDAO<userLogin> {
+}
