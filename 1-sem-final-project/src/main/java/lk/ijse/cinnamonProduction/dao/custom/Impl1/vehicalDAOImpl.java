f19e3e48e1d45b7f0570957777f12f806776b7a5
sachiniwathudura <sachiniwathudura@gmail.com>
2024-01-12 11:03:52 +0530

    vehicalDAO complete

--- 1-sem-final-project/src/main/java/lk/ijse/cinnamonProduction/dao/custom/Impl1/vehicalDAOImpl.java
+++ 1-sem-final-project/src/main/java/lk/ijse/cinnamonProduction/dao/custom/Impl1/vehicalDAOImpl.java
@@ -1,21 +1,19 @@
 package lk.ijse.cinnamonProduction.dao.custom.Impl1;
 
+import lk.ijse.cinnamonProduction.dao.SQLUtil;
 import lk.ijse.cinnamonProduction.dao.custom.vehicalDAO;
-import lk.ijse.cinnamonProduction.db.DbConnection;
+import lk.ijse.cinnamonProduction.dto.cinnamonGradesDto;
 import lk.ijse.cinnamonProduction.entity.vehical;
-import lk.ijse.cinnamonProduction.viewTDM.vehicalTm;
 //import lk.ijse.cinnamonProduction.dto.tm.vehicalTm;
 //import lk.ijse.cinnamonProduction.dto.vehical;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class vehicalModel implements vehicalDAO {
-    public static boolean saveVehical(vehical vehicalDto) throws SQLException {
+public class vehicalDAOImpl implements vehicalDAO {
+   /* public static boolean saveVehical(vehical vehicalDto) throws SQLException {
         Connection connection = DbConnection.getInstance().getConnection();
         String sql = " INSERT INTO vehical VALUES(?, ?)";
         PreparedStatement pstm = connection.prepareStatement(sql);
@@ -90,6 +88,70 @@ public class vehicalModel implements vehicalDAO {
         }
         return dtoList;
 
+    }*/
+
+    @Override
+    public List<vehical> getAll() throws SQLException, ClassNotFoundException {
+       // String sql = "SELECT * FROM vehical";
+       // PreparedStatement pstm = connection.prepareStatement(sql);
+
+        List<vehical> dtoList = new ArrayList<>();
+
+        ResultSet resultSet = SQLUtil.execute("SELECT * FROM vehical");
+
+        while (resultSet.next()) {
+            String vehicalNo = resultSet.getString(1);
+            String vehicalStatus = resultSet.getString(2);
+
+
+            var dto = new vehical(vehicalNo,vehicalStatus);
+            dtoList.add(dto);
+        }
+        return dtoList;
+    }
+
+    @Override
+    public boolean save(vehical entity) throws SQLException, ClassNotFoundException {
+        return SQLUtil.execute("INSERT INTO vehical VALUES(?, ?)",
+                entity.getVehicalNo(),entity.getVehicalStatus());
+    }
+
+    @Override
+    public boolean update(vehical entity) throws SQLException, ClassNotFoundException {
+        return SQLUtil.execute("UPDATE vehical SET  vehicalStatus= ? WHERE vehicalNo = ?",
+                entity.getVehicalStatus(),entity.getVehicalNo());
+    }
+
+    @Override
+    public boolean exist(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) throws SQLException, ClassNotFoundException {
+       boolean isDeleted= SQLUtil.execute("DELETE FROM vehical WHERE vehicalNo= ?",id);
+        return isDeleted;
+    }
+
+    @Override
+    public String generateID() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public vehical search(String id) throws SQLException, ClassNotFoundException {
+        
+        ResultSet resultSet = SQLUtil.execute("SELECT * FROM vehical WHERE vehicalNo = ?",id);
+
+        vehical entity = null;
+        if(resultSet.next()){
+
+            String vehicalNo = resultSet.getString(1);
+            String vehicalStatus = resultSet.getString(2);
+
+            entity = new vehical(vehicalNo,vehicalStatus);
+        }
+        return entity;
     }
 }
 
