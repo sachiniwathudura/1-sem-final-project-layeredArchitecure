a766ed15839bd196534e73cd1f4d054d4a2b811a
sachiniwathudura <sachiniwathudura@gmail.com>
2024-01-12 10:46:20 +0530

    companyDAO complete

--- 1-sem-final-project/src/main/java/lk/ijse/cinnamonProduction/dao/custom/Impl1/companyDAOImpl.java
+++ 1-sem-final-project/src/main/java/lk/ijse/cinnamonProduction/dao/custom/Impl1/companyDAOImpl.java
@@ -1,22 +1,77 @@
 package lk.ijse.cinnamonProduction.dao.custom.Impl1;
 
+import lk.ijse.cinnamonProduction.dao.SQLUtil;
 import lk.ijse.cinnamonProduction.dao.custom.companyDAO;
-import lk.ijse.cinnamonProduction.db.DbConnection;
+import lk.ijse.cinnamonProduction.dto.cinnamonGradesDto;
+import lk.ijse.cinnamonProduction.entity.cinnamonGrades;
 import lk.ijse.cinnamonProduction.entity.company;
-import lk.ijse.cinnamonProduction.viewTDM.companyTm;
 //import lk.ijse.cinnamonProduction.dto.company;
 //import lk.ijse.cinnamonProduction.dto.tm.companyTm;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class companyModel implements companyDAO {
+public class companyDAOImpl implements companyDAO {
+    @Override
+    public List<company> getAll() throws SQLException, ClassNotFoundException {
+        // Connection connection = DbConnection.getInstance().getConnection();
 
-    public  boolean deleteCompany(String companyId) throws SQLException {
+        // String sql = "SELECT * FROM company";
+        //PreparedStatement pstm = connection.prepareStatement(sql);
+
+        List<company> dtoList = new ArrayList<>();
+
+        ResultSet resultSet = SQLUtil.execute("SELECT * FROM company");
+
+        while (resultSet.next()) {
+            String companyId = resultSet.getString(1);
+            String companyName = resultSet.getString(2);
+            String companyEmail = resultSet.getString(3);
+
+            var dto = new company(companyId, companyName, companyEmail);
+            dtoList.add(dto);
+        }
+        return dtoList;
+    }
+
+    @Override
+    public boolean save(company entity) throws SQLException, ClassNotFoundException {
+        return SQLUtil.execute("INSERT INTO company VALUES(?, ?, ?)",
+                entity.getCompanyId(), entity.getCompanyName(), entity.getCompanyEmail());
+    }
+
+    @Override
+    public boolean update(company entity) throws SQLException, ClassNotFoundException {
+        return SQLUtil.execute("UPDATE company SET companyName = ?, companyEmail = ? WHERE companyId= ?",
+                entity.getCompanyName(), entity.getCompanyEmail(), entity.getCompanyId());
+    }
+
+    @Override
+    public boolean exist(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) throws SQLException, ClassNotFoundException {
+        SQLUtil.execute("DELETE FROM company WHERE companyId = ?", id);
+
+        return false;
+    }
+
+    @Override
+    public String generateID() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public company search(String id) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+}
+
+   /* public  boolean deleteCompany(String companyId) throws SQLException {
 
         Connection connection = DbConnection.getInstance().getConnection();
 
@@ -100,4 +155,5 @@ public class companyModel implements companyDAO {
 
     }
 
-}
+
+}*/
