4cf63b97ab2979e5355bb58bb75955e66297f0ad
sachiniwathudura <sachiniwathudura@gmail.com>
2024-01-12 10:52:35 +0530

    employeeDAO complete

--- 1-sem-final-project/src/main/java/lk/ijse/cinnamonProduction/dao/custom/employeeDAO.java
+++ 1-sem-final-project/src/main/java/lk/ijse/cinnamonProduction/dao/custom/employeeDAO.java
@@ -1,7 +1,25 @@
 package lk.ijse.cinnamonProduction.dao.custom;
 
 import lk.ijse.cinnamonProduction.dao.CrudDAO;
+import lk.ijse.cinnamonProduction.db.DbConnection;
 import lk.ijse.cinnamonProduction.entity.employee;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public interface employeeDAO extends CrudDAO<employee> {
+    public static int countEmployee() throws SQLException, SQLException {
+        int count = 0;
+        Connection connection = DbConnection.getInstance().getConnection();
+
+        String sql = "select empId,count(*) as count from employee group by empId";
+        PreparedStatement pstm = connection.prepareStatement(sql);
+        ResultSet resultSet = pstm.executeQuery();
+        while (resultSet.next()) {
+            count++;
+        }
+        return count;
+    }
 }
