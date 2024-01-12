f9f0907d8a47c31a25a282bae5cb77709a9072a5
sachiniwathudura <sachiniwathudura@gmail.com>
2024-01-12 10:52:07 +0530

    employeeFromController complete

--- 1-sem-final-project/src/main/java/lk/ijse/cinnamonProduction/dao/custom/Impl1/employeeManagementDAOImpl.java
+++ 1-sem-final-project/src/main/java/lk/ijse/cinnamonProduction/dao/custom/Impl1/employeeManagementDAOImpl.java
@@ -1,21 +1,20 @@
 package lk.ijse.cinnamonProduction.dao.custom.Impl1;
 
 
+import lk.ijse.cinnamonProduction.dao.SQLUtil;
 import lk.ijse.cinnamonProduction.dao.custom.employeeDAO;
-import lk.ijse.cinnamonProduction.db.DbConnection;
 //import lk.ijse.cinnamonProduction.dto.employee;
+import lk.ijse.cinnamonProduction.dto.cinnamonGradesDto;
 import lk.ijse.cinnamonProduction.entity.employee;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class employeeManagementModel implements employeeDAO {
+public class employeeManagementDAOImpl implements employeeDAO {
 
-    public static boolean deleteEmployee(String empId) throws SQLException {
+  /*  public static boolean deleteEmployee(String empId) throws SQLException {
         Connection connection = DbConnection.getInstance().getConnection();
 
         String sql = "DELETE FROM employee WHERE empId = ?";
@@ -118,5 +117,73 @@ public class employeeManagementModel implements employeeDAO {
             count++;
         }
         return  count;
+    }*/
+
+    @Override
+    public List<employee> getAll() throws SQLException, ClassNotFoundException {
+        List<employee> dtoList = new ArrayList<>();
+
+        ResultSet resultSet = SQLUtil.execute("SELECT * FROM employee");
+
+        while (resultSet.next()) {
+            String empId = resultSet.getString(1);
+            String empName = resultSet.getString(2);
+            String empAddress = resultSet.getString(3);
+            String empTeleNo = resultSet.getString(4);
+            String empStatus = resultSet.getString(5);
+
+            var dto = new employee(empId,empName,empAddress, empTeleNo,empStatus);
+            dtoList.add(dto);
+        }
+        return dtoList;
+
+    }
+
+    @Override
+    public boolean save(employee entity) throws SQLException, ClassNotFoundException {
+        return SQLUtil.execute("INSERT INTO employee VALUES(?, ?, ?, ?, ?)",
+                entity.getEmpId(),entity.getEmpName(),entity.getEmpAddress(),entity.getEmpTeleNo(),entity.getEmpStatus());
+    }
+
+    @Override
+    public boolean update(employee entity) throws SQLException, ClassNotFoundException {
+        return SQLUtil.execute("UPDATE employee SET empName = ?, empAddress = ?,  empTeleNo = ?, empStatus = ? WHERE empId=?",
+                entity.getEmpName(),entity.getEmpAddress(),entity.getEmpTeleNo(),entity.getEmpStatus(),entity.getEmpId());
+    }
+
+    @Override
+    public boolean exist(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) throws SQLException, ClassNotFoundException {
+        boolean isDeleted=SQLUtil.execute("DELETE FROM employee WHERE empId = ?",id);
+        return isDeleted;
+    }
+
+    @Override
+    public String generateID() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public employee search(String id) throws SQLException, ClassNotFoundException {
+        ResultSet resultSet = SQLUtil.execute("SELECT * FROM employee WHERE empId = ?");
+
+        employee entity = null;
+        if(resultSet.next()){
+
+            String empId = resultSet.getString(1);
+            String empName = resultSet.getString(2);
+            String empAddress = resultSet.getString(3);
+            String empTeleNo = resultSet.getString(4);
+            String empStatus = resultSet.getString(5);
+
+            entity = new employee(empId,empName,empAddress,empTeleNo,empStatus);
+
+        }
+        return entity;
+
     }
 }
