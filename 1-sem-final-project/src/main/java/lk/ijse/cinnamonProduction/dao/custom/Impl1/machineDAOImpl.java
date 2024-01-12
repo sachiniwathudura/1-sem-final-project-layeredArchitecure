13f08c26cc2f27461488e663f67aa926ead68a1e
sachiniwathudura <sachiniwathudura@gmail.com>
2024-01-12 10:55:36 +0530

    machineDAO complete

--- 1-sem-final-project/src/main/java/lk/ijse/cinnamonProduction/dao/custom/Impl1/machineDAOImpl.java
+++ 1-sem-final-project/src/main/java/lk/ijse/cinnamonProduction/dao/custom/Impl1/machineDAOImpl.java
@@ -1,22 +1,21 @@
 package lk.ijse.cinnamonProduction.dao.custom.Impl1;
 
+import lk.ijse.cinnamonProduction.dao.SQLUtil;
 import lk.ijse.cinnamonProduction.dao.custom.machineDAO;
-import lk.ijse.cinnamonProduction.db.DbConnection;
+import lk.ijse.cinnamonProduction.dto.cinnamonGradesDto;
+import lk.ijse.cinnamonProduction.entity.cinnamonGrades;
 import lk.ijse.cinnamonProduction.entity.machine;
-import lk.ijse.cinnamonProduction.viewTDM.machineTm;
 //import lk.ijse.cinnamonProduction.dto.machine;
 //import lk.ijse.cinnamonProduction.dto.tm.machineTm;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class machineModel implements machineDAO {
+public class machineDAOImpl implements machineDAO {
 
-    public static boolean deleteMachine(String machineId) throws SQLException {
+  /*  public static boolean deleteMachine(String machineId) throws SQLException {
 
         Connection connection = DbConnection.getInstance().getConnection();
 
@@ -95,5 +94,69 @@ public class machineModel implements machineDAO {
             dto = new machine(machineId,machineName,machineStatus);
         }
         return dto;
+    }*/
+
+    @Override
+    public List<machine> getAll() throws SQLException, ClassNotFoundException {
+
+        List<machine> dtoList = new ArrayList<>();
+
+        ResultSet resultSet = SQLUtil.execute("SELECT * FROM machineDetails");
+
+        while (resultSet.next()) {
+            String machineId = resultSet.getString(1);
+            String machineName = resultSet.getString(2);
+            String machineStatus = resultSet.getString(3);
+
+            var entity = new machine(machineId,machineName,machineStatus);
+            dtoList.add(entity);
+        }
+        return dtoList;
+
+    }
+
+    @Override
+    public boolean save(machine entity) throws SQLException, ClassNotFoundException {
+        return SQLUtil.execute("INSERT INTO machineDetails VALUES(?, ?, ?)",
+                entity.getMachineId(),entity.getMachineName(),entity.getMachineStatus());
+    }
+
+    @Override
+    public boolean update(machine entity) throws SQLException, ClassNotFoundException {
+        return SQLUtil.execute("UPDATE machineDetails SET machineName = ?, machineStatus = ? WHERE machineId =?",
+                entity.getMachineName(),entity.getMachineStatus(),entity.getMachineId());
+    }
+
+    @Override
+    public boolean exist(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) throws SQLException, ClassNotFoundException {
+      boolean isDeleted=  SQLUtil.execute("DELETE FROM machineDetails WHERE machineId = ?",id);
+        return isDeleted;
+    }
+
+    @Override
+    public String generateID() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public machine search(String id) throws SQLException, ClassNotFoundException {
+
+        ResultSet resultSet = SQLUtil.execute("SELECT * FROM machineDetails WHERE machineId = ?",id);
+
+        machine entity = null;
+        if(resultSet.next()){
+
+            String machineId = resultSet.getString(1);
+            String machineName = resultSet.getString(2);
+            String machineStatus = resultSet.getString(3);
+
+            entity = new machine(machineId,machineName,machineStatus);
+        }
+        return entity;
     }
 }
