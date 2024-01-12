148f7a1a82fc6942db3f74588115f0e5a2d6b4a7
sachiniwathudura <sachiniwathudura@gmail.com>
2024-01-12 11:01:44 +0530

    userLoginDAO complete

--- 1-sem-final-project/src/main/java/lk/ijse/cinnamonProduction/dao/custom/Impl1/userLoginDAOImpl.java
+++ 1-sem-final-project/src/main/java/lk/ijse/cinnamonProduction/dao/custom/Impl1/userLoginDAOImpl.java
@@ -1,16 +1,17 @@
 package lk.ijse.cinnamonProduction.dao.custom.Impl1;
 
-import lk.ijse.cinnamonProduction.db.DbConnection;
+import lk.ijse.cinnamonProduction.dao.SQLUtil;
+import lk.ijse.cinnamonProduction.dao.custom.userLoginDAO;
+import lk.ijse.cinnamonProduction.dto.cinnamonGradesDto;
 import lk.ijse.cinnamonProduction.entity.userLogin;
 //import lk.ijse.cinnamonProduction.dto.userLogin;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 
-public class userLoginModel {
+public class userLoginDAOImpl implements userLoginDAO {
 
-    public static boolean saveUser(userLogin userLoginDto) throws SQLException {
+    /*public static boolean saveUser(userLogin userLoginDto) throws SQLException {
           Connection connection = DbConnection.getInstance().getConnection();
           String sql = " INSERT INTO user VALUES(?, ?, ?, ?)";
           PreparedStatement pstm = connection.prepareStatement(sql);
@@ -50,5 +51,43 @@ public class userLoginModel {
         pstm.setString(4, userLoginDto.getUserId());
 
         return pstm.executeUpdate() > 0;
+    }*/
+
+    @Override
+    public List<userLogin> getAll() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean save(userLogin entity) throws SQLException, ClassNotFoundException {
+        return SQLUtil.execute("INSERT INTO user VALUES(?, ?, ?, ?)",
+                entity.getUserId(),entity.getUserName(),entity.getUserEmail(),entity.getPassword());
+    }
+
+    @Override
+    public boolean update(userLogin entity) throws SQLException, ClassNotFoundException {
+        return SQLUtil.execute("UPDATE user SET userName = ?, userEmail = ?, password = ? WHERE userId = ?",
+                entity.getUserName(),entity.getUserEmail(),entity.getPassword(),entity.getUserId());
+    }
+
+    @Override
+    public boolean exist(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) throws SQLException, ClassNotFoundException {
+        SQLUtil.execute("DELETE FROM user WHERE userId = ?",id);
+        return false;
+    }
+
+    @Override
+    public String generateID() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public userLogin search(String id) throws SQLException, ClassNotFoundException {
+        return null;
     }
 }
